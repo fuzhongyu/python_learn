ebc04726bbeb97263c58df585090a73b508b4127
fuzhongyu <fuzhongyu2@163.com>
2018-11-02 20:16:03 +0800

    commit

--- src/com/fucai/QqRobot.py
+++ src/com/fucai/QqRobot.py
@@ -9,7 +9,7 @@ __author__ = 'fucai'
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    if '赵方' in contact.name:
+    if 'aaa' in contact.name:
         if '/Emoji128054' in content or '狗' in content or 'dog' in content:
             bot.SendTo(contact, 'message1')
         elif '娘' in content or 'niang' in content or '炮' in content or 'pao' in content:
