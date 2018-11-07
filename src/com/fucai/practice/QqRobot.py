899c3af40e50adbe369f0c789c5b64be7a37ecef
fuzhongyu <fuzhongyu2@163.com>
2018-11-07 09:28:12 +0800

    Default Changelist

--- src/com/fucai/practice/QqRobot.py
+++ src/com/fucai/practice/QqRobot.py
@@ -9,14 +9,11 @@ __author__ = 'fucai'
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    # if 'XXX' in contact.name:
-    #     if '/Emoji128054' in content or '狗' in content or 'dog' in content:
-    #         bot.SendTo(contact, 'message1')
-    #     elif '娘' in content or 'niang' in content or '炮' in content or 'pao' in content:
-    #         bot.SendTo(contact, 'message2')
-
-    if '马' in contact.name:
-        bot.SendTo(contact, '马威峰')
+    if 'XXX' in contact.name:
+        if '/Emoji128054' in content or '狗' in content or 'dog' in content:
+            bot.SendTo(contact, 'message1')
+        elif '娘' in content or 'niang' in content or '炮' in content or 'pao' in content:
+            bot.SendTo(contact, 'message2')
 
 if __name__ == '__main__':
     RunBot()
